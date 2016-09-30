1f4bb029c2dfcb3bdcb798659d6b2d95d7bf8ab6
lhhxsm <lhhxsm@gmail.com>
2016-09-30 14:54:46 +0800

    删除一些没有用的工具

--- ExZoneLib/src/main/java/com/exzone/lib/base/BaseApplication.java
+++ ExZoneLib/src/main/java/com/exzone/lib/base/BaseApplication.java
@@ -2,8 +2,12 @@ package com.exzone.lib.base;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Handler;
+import android.os.Process;
 
-import com.exzone.lib.util.AppContextUtil;
+import com.exzone.lib.util.Logger;
 
 /**
  * 作者:李鸿浩
@@ -12,18 +16,28 @@ import com.exzone.lib.util.AppContextUtil;
  */
 public class BaseApplication extends Application {
 
-    private static Context sContext;
+    public static Context sContext;
+    public static Handler sHandler;
+    public static int sMainTid;
 
     @Override
     public void onCreate() {
+        Logger.initLog(true);// 打开Log输出
+        sContext = getApplicationContext();
+        sHandler = new Handler();
+        sMainTid = Process.myTid();
+        initWidthAndHeight();
         super.onCreate();
-        sContext = this;
-        AppContextUtil.init(this);
-
     }
 
-    // 获取ApplicationContext
-    public static Context getContext() {
-        return sContext;
+    /**
+     * 获取屏幕长度高度
+     */
+    private void initWidthAndHeight() {
+        // 防止被系统字体改变默认得字体大小
+        Resources resource = getResources();
+        Configuration c = resource.getConfiguration();
+        c.fontScale = 1.0f;
+        resource.updateConfiguration(c, resource.getDisplayMetrics());
     }
 }
