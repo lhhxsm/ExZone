a93fb0bd425e61dd409e6313d477b4d874809e44
lhh <lhhxsm@gmail.com>
2017-03-12 18:29:43 +0800

    2017.3.12 添加多点触控的图片控件

--- ExZoneDome/src/main/java/com/exzone/demo/MainActivity.java
+++ ExZoneDome/src/main/java/com/exzone/demo/MainActivity.java
@@ -1,10 +1,8 @@
 package com.exzone.demo;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.exzone.lib.view.OutlineTextView;
 /**
  * 作者:李鸿浩
  * 描述:
@@ -12,16 +10,9 @@ import com.exzone.lib.view.OutlineTextView;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private OutlineTextView mOutlineTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mOutlineTextView = (OutlineTextView) findViewById(R.id.text);
-        OutlineTextView textView = (OutlineTextView) findViewById(R.id.text0);
-        mOutlineTextView.setShadowLayer(0, 5, 5, Color.BLACK);
-        textView.setShadowLayer(0, 0, 0, Color.BLACK);
     }
 }
