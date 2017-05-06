06b7b285ead878fd7504692cecf0188aa6da7bad
lihonghao <lhhxsm@gmail.com>
2017-05-06 18:15:15 +0800

    2017.5.6  IOC

---
+++ BaseLibrary/src/main/java/com/android/lib/utils/NetUtils.java
@@ -0,0 +1,28 @@
+package com.android.lib.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * 作者:李鸿浩
+ * 描述:
+ * 时间:2017/5/4.
+ */
+public class NetUtils {
+
+  /**
+   * 是否有网络
+   */
+  public static boolean isAvailable(Context context) {
+    try {
+      ConnectivityManager manager =
+          (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+      NetworkInfo info = manager.getActiveNetworkInfo();
+      return info != null && info.isConnected();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+}
